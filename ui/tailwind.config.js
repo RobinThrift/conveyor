a2a941131e20118d288bcc8e6ecfa93c7b2fc619
Robin Thrift <services@robinthrift.com>
2024-11-18 12:33:29 +0100

    fix(Styles): Fix CSS vars to TW conversion

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -10,61 +10,62 @@ export default {
             colors: {
                 primary: {
                     "extra-light":
-                        "rgba(var(--color-primary-extra-light), <alpha-value>)",
-                    light: "rgba(var(--color-primary-light), <alpha-value>)",
-                    DEFAULT: "rgba(var(--color-primary), <alpha-value>)",
-                    dark: "rgba(var(--color-primary-dark), <alpha-value>)",
+                        "rgba(var(--color-primary-extra-light) / <alpha-value>)",
+                    light: "rgba(var(--color-primary-light) / <alpha-value>)",
+                    DEFAULT: "rgba(var(--color-primary) / <alpha-value>)",
+                    dark: "rgba(var(--color-primary-dark) / <alpha-value>)",
                     "extra-dark":
-                        "rgba(var(--color-primary-extra-dark), <alpha-value>)",
+                        "rgba(var(--color-primary-extra-dark) / <alpha-value>)",
                     contrast:
-                        "rgba(var(--color-primary-contrast), <alpha-value>)",
+                        "rgba(var(--color-primary-contrast) / <alpha-value>)",
                 },
 
                 info: {
                     "extra-light":
-                        "rgba(var(--color-info-extra-light), <alpha-value>)",
-                    light: "rgba(var(--color-info-light), <alpha-value>)",
-                    DEFAULT: "rgba(var(--color-info), <alpha-value>)",
-                    dark: "rgba(var(--color-info-dark), <alpha-value>)",
+                        "rgba(var(--color-info-extra-light) / <alpha-value>)",
+                    light: "rgba(var(--color-info-light) / <alpha-value>)",
+                    DEFAULT: "rgba(var(--color-info) / <alpha-value>)",
+                    dark: "rgba(var(--color-info-dark) / <alpha-value>)",
                     "extra-dark":
-                        "rgba(var(--color-info-extra-dark), <alpha-value>)",
-                    contrast: "rgba(var(--color-info-contrast), <alpha-value>)",
+                        "rgba(var(--color-info-extra-dark) / <alpha-value>)",
+                    contrast:
+                        "rgba(var(--color-info-contrast) / <alpha-value>)",
                 },
 
                 success: {
                     "extra-light":
-                        "rgba(var(--color-success-extra-light), <alpha-value>)",
-                    light: "rgba(var(--color-success-light), <alpha-value>)",
-                    DEFAULT: "rgba(var(--color-success), <alpha-value>)",
-                    dark: "rgba(var(--color-success-dark), <alpha-value>)",
+                        "rgba(var(--color-success-extra-light) / <alpha-value>)",
+                    light: "rgba(var(--color-success-light) / <alpha-value>)",
+                    DEFAULT: "rgba(var(--color-success) / <alpha-value>)",
+                    dark: "rgba(var(--color-success-dark) / <alpha-value>)",
                     "extra-dark":
-                        "rgba(var(--color-success-extra-dark), <alpha-value>)",
+                        "rgba(var(--color-success-extra-dark) / <alpha-value>)",
                     contrast:
-                        "rgba(var(--color-success-contrast), <alpha-value>)",
+                        "rgba(var(--color-success-contrast) / <alpha-value>)",
                 },
 
                 danger: {
                     "extra-light":
-                        "rgba(var(--color-danger-extra-light), <alpha-value>)",
-                    light: "rgba(var(--color-danger-light), <alpha-value>)",
-                    DEFAULT: "rgba(var(--color-danger), <alpha-value>)",
-                    dark: "rgba(var(--color-danger-dark), <alpha-value>)",
+                        "rgba(var(--color-danger-extra-light) / <alpha-value>)",
+                    light: "rgba(var(--color-danger-light) / <alpha-value>)",
+                    DEFAULT: "rgba(var(--color-danger) / <alpha-value>)",
+                    dark: "rgba(var(--color-danger-dark) / <alpha-value>)",
                     "extra-dark":
-                        "rgba(var(--color-danger-extra-dark), <alpha-value>)",
+                        "rgba(var(--color-danger-extra-dark) / <alpha-value>)",
                     contrast:
-                        "rgba(var(--color-danger-contrast), <alpha-value>)",
+                        "rgba(var(--color-danger-contrast) / <alpha-value>)",
                 },
 
                 subtle: {
                     "extra-light":
-                        "rgba(var(--color-subtle-extra-light), <alpha-value>)",
-                    light: "rgba(var(--color-subtle-light), <alpha-value>)",
-                    DEFAULT: "rgba(var(--color-subtle), <alpha-value>)",
-                    dark: "rgba(var(--color-subtle-dark), <alpha-value>)",
+                        "rgba(var(--color-subtle-extra-light) / <alpha-value>)",
+                    light: "rgba(var(--color-subtle-light) / <alpha-value>)",
+                    DEFAULT: "rgba(var(--color-subtle) / <alpha-value>)",
+                    dark: "rgba(var(--color-subtle-dark) / <alpha-value>)",
                     "extra-dark":
-                        "rgba(var(--color-subtle-extra-dark), <alpha-value>)",
+                        "rgba(var(--color-subtle-extra-dark) / <alpha-value>)",
                     contrast:
-                        "rgba(var(--color-subtle-contrast), <alpha-value>)",
+                        "rgba(var(--color-subtle-contrast) / <alpha-value>)",
                 },
 
                 text: "rgba(var(--text-color) / <alpha-value>)",
