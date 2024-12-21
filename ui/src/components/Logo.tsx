256031950eeba8854be635ba767defb7132d9675
Robin Thrift <services@robinthrift.com>
2024-12-21 07:54:27 +0100

    remove: Remove components that are no longer required

--- ui/src/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-import LogoURL from "../../public/logo.svg?url"
-
-export function Logo({ className }: { className?: string }) {
-    return <img src={LogoURL} alt="logo" className={className} />
-}
