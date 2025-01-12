2b1b96fae2946c8754893d5f305db6b101b1d7af
Robin Thrift <services@robinthrift.com>
2025-01-12 14:59:42 +0100

    refactor(UI/Login): Refactor Login Page UI

--- ui/src/pages/Login/ChangePasswordPage.tsx
+++ ui/src/pages/Login/ChangePasswordPage.tsx
@@ -23,131 +23,153 @@ export function ChangePasswordPage(props: ChangePasswordPageProps) {
     let t = useT("pages/LoginChangePassword")
 
     return (
-        <div className="h-screen px-2 sm:px-4">
-            <header className="login-header">
-                <div className="logo">Belt</div>
+        <div className="login-page">
+            <div className="login-page-bg" aria-hidden>
+                <div className="spot-3" />
+                <div className="spot-2" />
+                <div className="spot-1" />
+                <div className="noise" />
+            </div>
+
+            <div className="logo" aria-hidden>
+                <span className="eeee">eeeeeeeeeeeeeeee</span>
+                <span>
+                    B<em>e</em>lt
+                </span>
+            </div>
+
+            <header className="login-page-header">
                 <SelectMode className="mode-select" />
             </header>
-            <div className="login-window">
-                <h1 className="!text-6xl">{t.Title}</h1>
 
-                <Form.Root
-                    action={`${baseURL}/auth/change_password`}
-                    method="post"
-                    className="p-4"
-                >
-                    <input
-                        type="hidden"
-                        id="belt.csrf.token"
-                        name="belt.csrf.token"
-                        defaultValue={csrfToken}
-                    />
+            <div className="login-window-positioner">
+                <div className="login-window">
+                    <h1 className="!text-6xl">{t.Title}</h1>
 
-                    <input
-                        type="hidden"
-                        id="redirect_url"
-                        name="redirect_url"
-                        defaultValue={props.redirectURL}
-                    />
+                    <Form.Root
+                        action={`${baseURL}/auth/change_password`}
+                        method="post"
+                        className="p-4"
+                    >
+                        <input
+                            type="hidden"
+                            id="belt.csrf.token"
+                            name="belt.csrf.token"
+                            defaultValue={csrfToken}
+                        />
 
-                    <div className="space-y-2">
-                        <Form.Field
-                            name="current_password"
-                            aria-label={t.CurrentPasswordLabel}
-                            className="input-field"
-                            serverInvalid={
-                                !!props.validationErrors?.current_password
-                            }
-                        >
-                            <Form.Label htmlFor="current_password">
-                                {t.CurrentPasswordLabel}
-                            </Form.Label>
+                        <input
+                            type="hidden"
+                            id="redirect_url"
+                            name="redirect_url"
+                            defaultValue={props.redirectURL}
+                        />
 
-                            <Password weight="bold" size={24} />
+                        <div className="space-y-2">
+                            <Form.Field
+                                name="current_password"
+                                aria-label={t.CurrentPasswordLabel}
+                                className="input-field"
+                                serverInvalid={
+                                    !!props.validationErrors?.current_password
+                                }
+                            >
+                                <Form.Label htmlFor="current_password">
+                                    {t.CurrentPasswordLabel}
+                                </Form.Label>
 
-                            <Form.Control asChild>
-                                <input
-                                    type="password"
-                                    name="current_password"
-                                    id="current_password"
-                                    autoComplete="current_password"
-                                    placeholder={t.CurrentPasswordLabel}
-                                    required
-                                />
-                            </Form.Control>
-                        </Form.Field>
+                                <Password weight="bold" size={24} />
 
-                        <Form.Field
-                            name="new_password"
-                            aria-label={t.NewPasswordLabel}
-                            className="input-field"
-                            serverInvalid={
-                                !!props.validationErrors?.new_password
-                            }
-                        >
-                            <Form.Label htmlFor="new_password">
-                                {t.NewPasswordLabel}
-                            </Form.Label>
+                                <Form.Control asChild>
+                                    <input
+                                        type="password"
+                                        name="current_password"
+                                        id="current_password"
+                                        autoComplete="current_password"
+                                        placeholder={t.CurrentPasswordLabel}
+                                        required
+                                    />
+                                </Form.Control>
+                            </Form.Field>
 
-                            <Password weight="bold" size={24} />
+                            <Form.Field
+                                name="new_password"
+                                aria-label={t.NewPasswordLabel}
+                                className="input-field"
+                                serverInvalid={
+                                    !!props.validationErrors?.new_password
+                                }
+                            >
+                                <Form.Label htmlFor="new_password">
+                                    {t.NewPasswordLabel}
+                                </Form.Label>
 
-                            <Form.Control asChild>
-                                <input
-                                    type="password"
-                                    name="new_password"
-                                    id="new_password"
-                                    autoComplete="new_password"
-                                    placeholder={t.NewPasswordLabel}
-                                    required
-                                />
-                            </Form.Control>
-                        </Form.Field>
+                                <Password weight="bold" size={24} />
 
-                        <Form.Field
-                            name="repeate_new_password"
-                            aria-label={t.RepeatNewPasswordLabel}
-                            className="input-field"
-                            serverInvalid={
-                                !!props.validationErrors?.repeat_new_password
-                            }
-                        >
-                            <Form.Label htmlFor="repeate_new_password">
-                                {t.RepeatNewPasswordLabel}
-                            </Form.Label>
+                                <Form.Control asChild>
+                                    <input
+                                        type="password"
+                                        name="new_password"
+                                        id="new_password"
+                                        autoComplete="new_password"
+                                        placeholder={t.NewPasswordLabel}
+                                        required
+                                    />
+                                </Form.Control>
+                            </Form.Field>
 
-                            <Password weight="bold" size={24} />
+                            <Form.Field
+                                name="repeate_new_password"
+                                aria-label={t.RepeatNewPasswordLabel}
+                                className="input-field"
+                                serverInvalid={
+                                    !!props.validationErrors
+                                        ?.repeat_new_password
+                                }
+                            >
+                                <Form.Label htmlFor="repeate_new_password">
+                                    {t.RepeatNewPasswordLabel}
+                                </Form.Label>
 
-                            <Form.Control asChild>
-                                <input
-                                    type="password"
-                                    name="repeate_new_password"
-                                    id="repeate_new_password"
-                                    autoComplete="repeate_new_password"
-                                    placeholder={t.RepeatNewPasswordLabel}
-                                    required
-                                />
-                            </Form.Control>
-                        </Form.Field>
+                                <Password weight="bold" size={24} />
 
-                        {props.validationErrors &&
-                            Object.values(props.validationErrors).map((v) => (
-                                <div key={v} className="mt-5 field-message">
-                                    {t[v as keyof typeof t] ?? v}
-                                </div>
-                            ))}
+                                <Form.Control asChild>
+                                    <input
+                                        type="password"
+                                        name="repeate_new_password"
+                                        id="repeate_new_password"
+                                        autoComplete="repeate_new_password"
+                                        placeholder={t.RepeatNewPasswordLabel}
+                                        required
+                                    />
+                                </Form.Control>
+                            </Form.Field>
 
-                        <div className="flex items-center justify-end mt-4">
-                            <Form.Submit asChild>
-                                <button
-                                    className="btn primary lg w-full"
-                                    type="submit"
-                                >
-                                    {t.ChangePasswordButton}
-                                </button>
-                            </Form.Submit>
+                            {props.validationErrors &&
+                                Object.values(props.validationErrors).map(
+                                    (v) => (
+                                        <div
+                                            key={v}
+                                            className="mt-5 field-message"
+                                        >
+                                            {t[v as keyof typeof t] ?? v}
+                                        </div>
+                                    ),
+                                )}
+
+                            <div className="flex items-center justify-end mt-4">
+                                <Form.Submit asChild>
+                                    <button
+                                        className="btn primary lg w-full"
+                                        type="submit"
+                                    >
+                                        {t.ChangePasswordButton}
+                                    </button>
+                                </Form.Submit>
+                            </div>
                         </div>
-                    </div>
-                </Form.Root>
+                    </Form.Root>
+                </div>
             </div>
             <footer className="login-footer">
                 <BuildInfo />
--- ui/src/pages/Login/LoginPage.tsx
+++ ui/src/pages/Login/LoginPage.tsx
@@ -20,98 +20,114 @@ export function LoginPage(props: LoginPageProps) {
     let t = useT("pages/Login")
 
     return (
-        <div className="h-screen px-2 sm:px-4">
-            <header className="login-header">
-                <div className="logo">Belt</div>
+        <div className="login-page">
+            <div className="login-page-bg" aria-hidden>
+                <div className="spot-3" />
+                <div className="spot-2" />
+                <div className="spot-1" />
+                <div className="noise" />
+            </div>
+
+            <div className="logo" aria-hidden>
+                <span className="eeee">eeeeeeeeeeeeeeee</span>
+                <span>
+                    B<em>e</em>lt
+                </span>
+            </div>
+
+            <header className="login-page-header">
                 <SelectMode className="mode-select" />
             </header>
-            <div className="login-window">
-                <h1>{t.Title}</h1>
 
-                <Form.Root
-                    action={`${baseURL}/login`}
-                    method="post"
-                    className="p-4"
-                >
-                    <input
-                        type="hidden"
-                        id="belt.csrf.token"
-                        name="belt.csrf.token"
-                        defaultValue={csrfToken}
-                    />
+            <div className="login-window-positioner">
+                <div className="login-window">
+                    <h1>{t.Title}</h1>
 
-                    <input
-                        type="hidden"
-                        id="redirect_url"
-                        name="redirect_url"
-                        defaultValue={props.redirectURL}
-                    />
+                    <Form.Root
+                        action={`${baseURL}/login`}
+                        method="post"
+                        className="p-4"
+                    >
+                        <input
+                            type="hidden"
+                            id="belt.csrf.token"
+                            name="belt.csrf.token"
+                            defaultValue={csrfToken}
+                        />
 
-                    <div className="space-y-2">
-                        <Form.Field
-                            name="username"
-                            aria-label={t.UsernameLabel}
-                            className="input-field"
-                        >
-                            <Form.Label htmlFor="username">
-                                {t.UsernameLabel}
-                            </Form.Label>
+                        <input
+                            type="hidden"
+                            id="redirect_url"
+                            name="redirect_url"
+                            defaultValue={props.redirectURL}
+                        />
 
-                            <User weight="bold" size={24} />
-
-                            <Form.Control asChild>
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    autoComplete="username"
-                                    required={true}
-                                    placeholder={t.UsernameLabel}
-                                />
-                            </Form.Control>
-                        </Form.Field>
-
-                        <Form.Field
-                            name="password"
-                            aria-label={t.PasswordLabel}
-                            className="input-field"
-                        >
-                            <Form.Label htmlFor="username">
-                                {t.PasswordLabel}
-                            </Form.Label>
-
-                            <Password weight="bold" size={24} />
-
-                            <Form.Control asChild>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    autoComplete="password"
-                                    required={true}
-                                    placeholder={t.PasswordLabel}
-                                />
-                            </Form.Control>
-                        </Form.Field>
-                    </div>
-
-                    {props.validationErrors?.form && (
-                        <div className="mt-4 field-message">
-                            {props.validationErrors.form}
-                        </div>
-                    )}
-
-                    <div className="flex items-center justify-end mt-4">
-                        <Form.Submit asChild>
-                            <button
-                                className="btn primary lg w-full"
-                                type="submit"
+                        <div className="space-y-2">
+                            <Form.Field
+                                name="username"
+                                aria-label={t.UsernameLabel}
+                                className="input-field"
                             >
-                                {t.LoginButton}
-                            </button>
-                        </Form.Submit>
-                    </div>
-                </Form.Root>
+                                <Form.Label htmlFor="username">
+                                    {t.UsernameLabel}
+                                </Form.Label>
+
+                                <User weight="bold" size={24} />
+
+                                <Form.Control asChild>
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        id="username"
+                                        autoComplete="username"
+                                        required={true}
+                                        placeholder={t.UsernameLabel}
+                                    />
+                                </Form.Control>
+                            </Form.Field>
+
+                            <Form.Field
+                                name="password"
+                                aria-label={t.PasswordLabel}
+                                className="input-field"
+                            >
+                                <Form.Label htmlFor="username">
+                                    {t.PasswordLabel}
+                                </Form.Label>
+
+                                <Password weight="bold" size={24} />
+
+                                <Form.Control asChild>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        autoComplete="password"
+                                        required={true}
+                                        placeholder={t.PasswordLabel}
+                                    />
+                                </Form.Control>
+                            </Form.Field>
+                        </div>
+
+                        {props.validationErrors?.form && (
+                            <div className="mt-4 field-message">
+                                {props.validationErrors.form}
+                            </div>
+                        )}
+
+                        <div className="flex items-center justify-end mt-4">
+                            <Form.Submit asChild>
+                                <button
+                                    className="btn primary lg w-full"
+                                    type="submit"
+                                >
+                                    {t.LoginButton}
+                                </button>
+                            </Form.Submit>
+                        </div>
+                    </Form.Root>
+                </div>
             </div>
             <footer className="login-footer">
                 <BuildInfo />
