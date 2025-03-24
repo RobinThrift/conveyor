c0461d961fde5528306e83a998b602d0c16e1302
Robin Thrift <services@robinthrift.com>
2025-03-24 15:21:02 +0100

    remove: Remove old server code

--- internal/app/jobs.go
+++
@@ -1,7 +0,0 @@
-package app
-
-import (
-	"github.com/RobinThrift/belt/internal/jobs"
-)
-
-var jobFuncs = map[string]jobs.JobKindWithJSONData{}
--- internal/logging/attrs.go
+++
@@ -1,5 +0,0 @@
-package logging
-
-const RequestIDAttr = "request_id"
-const TraceIDAttr = "trace_id"
-const SpanIDAttr = "span_id"
