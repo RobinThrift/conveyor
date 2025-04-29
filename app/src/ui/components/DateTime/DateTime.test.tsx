30d396e8fdc0ed04626868c8e5ffdaeebf5b0550
Robin Thrift <services@robinthrift.com>
2025-04-29 15:43:34 +0200

    test: Fix relative DateTime test to be time independent

--- app/src/ui/components/DateTime/DateTime.test.tsx
+++ app/src/ui/components/DateTime/DateTime.test.tsx
@@ -1,13 +1,18 @@
 /// <reference types="@vitest/browser/providers/playwright" />
 import React from "react"
-import { expect, suite, test } from "vitest"
+import { expect, suite, test, vi } from "vitest"
 import { render } from "vitest-browser-react"
 
 import { currentDateTime } from "@/lib/i18n"
 import { DateTime } from "./DateTime"
 
 suite("ui/components/DateTime", () => {
-    test("relative", async () => {
+    test("relative", async ({ onTestFinished }) => {
+        onTestFinished(() => {
+            vi.useRealTimers()
+        })
+        vi.useFakeTimers()
+        vi.setSystemTime(new Date(2025, 4, 12, 12, 0, 0))
         let datetime = currentDateTime().subtract({ hours: 2 })
 
         let rendered = render(<DateTime date={datetime} relative={true} />)
