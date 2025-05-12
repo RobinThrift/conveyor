e1070ed0a31558980f1c6a1210a1dae9a53a5a3c
Robin Thrift <services@robinthrift.com>
2025-05-12 10:37:12 +0200

    test: Add virtualisation to Performance dev tool

--- app/src/ui/devtools/Performance.tsx
+++ app/src/ui/devtools/Performance.tsx
@@ -1,4 +1,11 @@
-import React, { useMemo, useSyncExternalStore } from "react"
+import React, { useCallback, useSyncExternalStore } from "react"
+import {
+    ListBox as AriaListBox,
+    ListBoxItem as AriaListBoxItem,
+    ListLayout as AriaListLayout,
+    Text as AriaText,
+    Virtualizer as AriaVirtualizer,
+} from "react-aria-components"
 
 import { Code } from "@/ui/components/Markdown/Code"
 
@@ -8,92 +15,116 @@ export function PerformanceDevTool() {
         _observer_getSnapshot,
     )
 
-    let items = useMemo(() => {
-        let els: React.ReactNode[] = []
-
-        for (let i = entries.length - 1; i >= 0; i--) {
-            if (entries[i].entryType === "measure") {
-                let entry = entries[i] as PerformanceMeasure
-                els.push(
-                    <Measurement
-                        key={`${entry.name}-${entry.startTime}`}
-                        name={entry.name}
-                        detail={entry.detail}
-                        duration={entry.duration}
-                        startTime={entry.startTime}
-                    />,
-                )
-                continue
-            }
-
-            if (entries[i].entryType === "resource") {
-                let entry = entries[i] as PerformanceResourceTiming
-                els.push(
-                    <Resource
-                        key={`${entry.name}-${entry.startTime}`}
-                        name={entry.name}
-                        duration={entry.duration}
-                        startTime={entry.startTime}
-                        initiatorType={entry.initiatorType}
-                        renderBlockingStatus={
-                            "renderBlockingStatus" in entry
-                                ? (entry.renderBlockingStatus as string)
-                                : "unknown"
-                        }
-                        decodedBodySize={entry.decodedBodySize}
-                    />,
-                )
-                continue
-            }
-
-            if (entries[i].entryType === "event") {
-                let entry = entries[i] as PerformanceEventTiming
-                els.push(
-                    <PerfEvent
-                        key={`${entry.name}-${entry.startTime}`}
-                        name={entry.name}
-                        duration={entry.duration}
-                        startTime={entry.startTime}
-                        processingStart={entry.processingStart}
-                        processingEnd={entry.processingEnd}
-                        target={entry.target}
-                    />,
-                )
-                continue
-            }
-
-            if (entries[i].entryType === "navigation") {
-                let entry = entries[i] as PerformanceNavigationTiming
-                els.push(
-                    <Navigation
-                        key={`${entry.name}-${entry.startTime}`}
-                        name={entry.name}
-                        duration={entry.duration}
-                        startTime={entry.startTime}
-                        decodedBodySize={entry.decodedBodySize}
-                        domComplete={entry.domComplete}
-                        domInteractive={entry.domInteractive}
-                    />,
-                )
-            }
-        }
-
-        return els
-    }, [entries])
+    let onClickClear = useCallback(() => {
+        _snapshot = []
+    }, [])
 
     return (
         <div className="dark min-h-full w-full relative overscroll-contain">
-            <header className="sticky top-0 left-0 right-0 p-2 text-xl font-mono text-text backdrop-blur-sm flex justify-between items-center rounded">
+            <header className="sticky top-0 left-0 right-0 p-2 text-xl font-mono text-text backdrop-blur-sm flex justify-between items-center rounded z-[100]">
                 Performance
+                <button
+                    type="button"
+                    className="text-sm cursor-pointer"
+                    onClick={onClickClear}
+                >
+                    [clear]
+                </button>
             </header>
-            <ul className="divide-y divide-subtle">{items}</ul>
+            <AriaVirtualizer
+                layout={AriaListLayout}
+                layoutOptions={{
+                    estimatedRowHeight: 105,
+                    gap: 0,
+                    padding: 0,
+                }}
+            >
+                <AriaListBox
+                    aria-label="Performance items"
+                    className="divide-y divide-subtle"
+                    items={reverse(entries)}
+                >
+                    {(entry) => (
+                        <Entry
+                            id={`${entry.name}-${entry.startTime}`}
+                            perfEntry={entry}
+                        />
+                    )}
+                </AriaListBox>
+            </AriaVirtualizer>
         </div>
     )
+}
+
+function* reverse<T>(a: Array<T>) {
+    for (let i = a.length - 1; i >= 0; i--) {
+        yield a[i]
+    }
 }
 
 let formatter = new Intl.DateTimeFormat("en-gb", {
     timeStyle: "medium",
 })
+
+function Entry({ perfEntry }: { perfEntry: PerformanceEntry; id: string }) {
+    if (perfEntry.entryType === "measure") {
+        let entry = perfEntry as PerformanceMeasure
+        return (
+            <Measurement
+                name={entry.name}
+                detail={entry.detail}
+                duration={entry.duration}
+                startTime={entry.startTime}
+            />
+        )
+    }
+
+    if (perfEntry.entryType === "resource") {
+        let entry = perfEntry as PerformanceResourceTiming
+        return (
+            <Resource
+                name={entry.name}
+                duration={entry.duration}
+                startTime={entry.startTime}
+                initiatorType={entry.initiatorType}
+                renderBlockingStatus={
+                    "renderBlockingStatus" in entry
+                        ? (entry.renderBlockingStatus as string)
+                        : "unknown"
+                }
+                decodedBodySize={entry.decodedBodySize}
+            />
+        )
+    }
+
+    if (perfEntry.entryType === "event") {
+        let entry = perfEntry as PerformanceEventTiming
+        return (
+            <PerfEvent
+                name={entry.name}
+                duration={entry.duration}
+                startTime={entry.startTime}
+                processingStart={entry.processingStart}
+                processingEnd={entry.processingEnd}
+                target={entry.target}
+            />
+        )
+    }
+
+    if (perfEntry.entryType === "navigation") {
+        let entry = perfEntry as PerformanceNavigationTiming
+        return (
+            <Navigation
+                name={entry.name}
+                duration={entry.duration}
+                startTime={entry.startTime}
+                decodedBodySize={entry.decodedBodySize}
+                domComplete={entry.domComplete}
+                domInteractive={entry.domInteractive}
+            />
+        )
+    }
+}
 
 const Measurement = React.memo(function Measurment({
     name,
@@ -102,10 +133,12 @@ const Measurement = React.memo(function Measurment({
     startTime,
 }: { name: string; detail?: any; duration: number; startTime: number }) {
     return (
-        <li className="p-4 hover:bg-surface-level-1">
+        <AriaListBoxItem textValue={name} className="p-4">
             <div className="flex items-center justify-between w-full text-subtle-light font-mono text-sm">
                 <span>
-                    {name}: {duration.toFixed(2)}ms
+                    <AriaText slot="label">
+                        {name}: {duration.toFixed(2)}ms
+                    </AriaText>
                 </span>
 
                 <time>
@@ -124,7 +157,7 @@ const Measurement = React.memo(function Measurment({
                     {JSON.stringify(detail)}
                 </Code>
             )}
-        </li>
+        </AriaListBoxItem>
     )
 })
 
@@ -144,12 +177,14 @@ const Resource = React.memo(function Resource({
     decodedBodySize: number
 }) {
     return (
-        <li className="p-4 hover:bg-surface-level-1">
+        <AriaListBoxItem textValue={name} className="p-4">
             <div className="flex items-center justify-between w-full text-subtle-light font-mono text-sm">
                 <span>
-                    {name.replace(window.location.href, "")}:{" "}
-                    {duration.toFixed(2)}ms
-                    {` - ${(decodedBodySize / 1000).toFixed(2)}kB`}
+                    <AriaText slot="label">
+                        {name.replace(window.location.href, "")}:{" "}
+                        {duration.toFixed(2)}ms
+                        {` - ${(decodedBodySize / 1000).toFixed(2)}kB`}
+                    </AriaText>
                 </span>
 
                 <time>
@@ -165,7 +200,7 @@ const Resource = React.memo(function Resource({
                 <span>Initiator: {initiatorType}</span>
                 <span>[{renderBlockingStatus}]</span>
             </div>
-        </li>
+        </AriaListBoxItem>
     )
 })
 
@@ -185,9 +220,13 @@ const PerfEvent = React.memo(function PerfEvent({
     target: any
 }) {
     return (
-        <li className="p-4 hover:bg-surface-level-1">
+        <AriaListBoxItem textValue={name} className="p-4">
             <div className="flex items-center justify-between w-full text-subtle-light font-mono text-sm">
-                <span>{`Event ${name}: ${duration.toFixed(2)}ms`}</span>
+                <span>
+                    <AriaText slot="label">
+                        {`Event ${name}: ${duration.toFixed(2)}ms`}
+                    </AriaText>
+                </span>
 
                 <time>
                     [
@@ -214,7 +253,7 @@ const PerfEvent = React.memo(function PerfEvent({
                     Log Target
                 </button>
             </div>
-        </li>
+        </AriaListBoxItem>
     )
 })
 
@@ -234,10 +273,12 @@ const Navigation = React.memo(function Resource({
     domInteractive: DOMHighResTimeStamp
 }) {
     return (
-        <li className="p-4 hover:bg-surface-level-1">
+        <AriaListBoxItem textValue={name} className="p-4">
             <div className="flex items-center justify-between w-full text-subtle-light font-mono text-sm">
                 <span>
-                    {`${name}: ${duration.toFixed(2)}ms - ${(decodedBodySize / 1000).toFixed(2)}kB`}
+                    <AriaText slot="label">
+                        {`${name}: ${duration.toFixed(2)}ms - ${(decodedBodySize / 1000).toFixed(2)}kB`}
+                    </AriaText>
                 </span>
 
                 <time>
@@ -254,7 +295,7 @@ const Navigation = React.memo(function Resource({
                 <span>-</span>
                 <span>{`DOM Interactive: ${(domInteractive.valueOf() - startTime).toFixed(2)}ms`}</span>
             </div>
-        </li>
+        </AriaListBoxItem>
     )
 })
 
@@ -326,6 +367,7 @@ observer.observe({
 
 observer.observe({
     type: "mark",
+    buffered: true,
 })
 
 function _observer_subscribe(callback: () => void) {
@@ -373,7 +415,13 @@ function _observer_getSnapshot() {
                 performance.measure(name, {
                     start: start.startTime,
                     end: entry.startTime,
-                    detail: start.detail ?? (entry as PerformanceMark).detail,
+                    detail:
+                        start.detail || (entry as PerformanceMark).detail
+                            ? {
+                                  ...(start.detail ?? {}),
+                                  ...((entry as PerformanceMark).detail ?? {}),
+                              }
+                            : undefined,
                 }),
             )
             continue
