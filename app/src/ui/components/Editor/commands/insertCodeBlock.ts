9e34806831b2bcdf8b98c06db2395fdaada62a43
Robin Thrift <services@robinthrift.com>
2026-02-11 14:23:00 +0100

    fix(UI/Editor): Fix code block insertion with text selection

--- app/src/ui/components/Editor/commands/insertCodeBlock.ts
+++ app/src/ui/components/Editor/commands/insertCodeBlock.ts
@@ -5,6 +5,7 @@ import type { EditorView } from "@codemirror/view"
 export function insertCodeBlock(view: EditorView) {
     let nodeType = syntaxTree(view.state).resolveInner(view.state.selection.main.from, -1)?.type
         ?.name
+
     if (nodeType === "FencedCode" || nodeType === "CodeText") {
         return
     }
@@ -15,13 +16,19 @@ export function insertCodeBlock(view: EditorView) {
             let to = range.to
             let hasSelection = from !== to
 
+            let start = "\n```"
+
+            if (hasSelection) {
+                start = "```\n"
+            }
+
             return {
                 changes: [
-                    { from: from, insert: "\n```" },
+                    { from: from, insert: start },
                     { from: to, insert: "\n```" },
                 ],
                 range: hasSelection
-                    ? EditorSelection.range(from - 3, to + 4)
+                    ? EditorSelection.range(from + 4, to + 4)
                     : EditorSelection.cursor(to + 4),
             }
         }),
